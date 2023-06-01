274fdaac4d4eae3bd4d35f0dbeaf90061e157906
Francisco Mena <115848687+Isco83@users.noreply.github.com>
2023-06-01 14:04:38 -0500

    Add files via upload

---
+++ LigaBasketbolNba2.js
@@ -0,0 +1,151 @@
+const LigaBasketbolNba = [    
+  { tipodeConferencia: "Este", divicion: "Atlantico", team: "Boston Celtics", jugadores:[
+      {tipoDePosicion: "Ala Pivot", name: "Jayson Christopher Tatum", estatura: "2.03 m", Edad: 25, draftdelaNBA: 2017, dorsal: 0},
+      {tipoDePosicion: "Ala Pivot", name: "Al Horford",               estatura: "2.06 m", Edad: 36, draftdelaNBA: 2007, dorsal: 42},
+      {tipoDePosicion: "Ala Pivot", name: "Grant Dean Williams",      estatura: "1.98 m", Edad: 24, draftdelaNBA: 2019, dorsal: 12},
+      {tipoDePosicion: "Ala Pivot", name: "Blake Austin Griffin",     estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2},
+      {tipoDePosicion: "Ala Pivot", name: "Danilo Gallinari",         estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2}
+
+  ]},
+  { tipodeConferencia: "Este", divicion: "Atlantico", team: "New Jersie Nets",jugadores:[
+    {tipoDePosicion: "Ala Pivot", name: "Jayson Christopher Tatum", estatura: "2.03 m", Edad: 25, draftdelaNBA: 2017, dorsal: 0},
+    {tipoDePosicion: "Ala Pivot", name: "Al Horford",               estatura: "2.06 m", Edad: 36, draftdelaNBA: 2007, dorsal: 42},
+    {tipoDePosicion: "Ala Pivot", name: "Grant Dean Williams",      estatura: "1.98 m", Edad: 24, draftdelaNBA: 2019, dorsal: 12},
+    {tipoDePosicion: "Ala Pivot", name: "Blake Austin Griffin",     estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2},
+    {tipoDePosicion: "Ala Pivot", name: "Danilo Gallinari",         estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2}
+
+]},
+  { tipodeConferencia: "Este", divicion: "Atlantico", team: "New York Nicks",jugadores:[
+    {tipoDePosicion: "Ala Pivot", name: "Jayson Christopher Tatum", estatura: "2.03 m", Edad: 25, draftdelaNBA: 2017, dorsal: 0},
+    {tipoDePosicion: "Ala Pivot", name: "Al Horford",               estatura: "2.06 m", Edad: 36, draftdelaNBA: 2007, dorsal: 42},
+    {tipoDePosicion: "Ala Pivot", name: "Grant Dean Williams",      estatura: "1.98 m", Edad: 24, draftdelaNBA: 2019, dorsal: 12},
+    {tipoDePosicion: "Ala Pivot", name: "Blake Austin Griffin",     estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2},
+    {tipoDePosicion: "Ala Pivot", name: "Danilo Gallinari",         estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2}
+
+]},
+  { tipodeConferencia: "Este", divicion: "Atlantico", team: "Philadelphia 76ers",jugadores:[
+    {tipoDePosicion: "Ala Pivot", name: "Jayson Christopher Tatum", estatura: "2.03 m", Edad: 25, draftdelaNBA: 2017, dorsal: 0},
+    {tipoDePosicion: "Ala Pivot", name: "Al Horford",               estatura: "2.06 m", Edad: 36, draftdelaNBA: 2007, dorsal: 42},
+    {tipoDePosicion: "Ala Pivot", name: "Grant Dean Williams",      estatura: "1.98 m", Edad: 24, draftdelaNBA: 2019, dorsal: 12},
+    {tipoDePosicion: "Ala Pivot", name: "Blake Austin Griffin",     estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2},
+    {tipoDePosicion: "Ala Pivot", name: "Danilo Gallinari",         estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2}
+
+]},
+  { tipodeConferencia: "Este", divicion: "Atlantico", team: "Toronto Raptors",jugadores:[
+    {tipoDePosicion: "Ala Pivot", name: "Jayson Christopher Tatum", estatura: "2.03 m", Edad: 25, draftdelaNBA: 2017, dorsal: 0},
+    {tipoDePosicion: "Ala Pivot", name: "Al Horford",               estatura: "2.06 m", Edad: 36, draftdelaNBA: 2007, dorsal: 42},
+    {tipoDePosicion: "Ala Pivot", name: "Grant Dean Williams",      estatura: "1.98 m", Edad: 24, draftdelaNBA: 2019, dorsal: 12},
+    {tipoDePosicion: "Ala Pivot", name: "Blake Austin Griffin",     estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2},
+    {tipoDePosicion: "Ala Pivot", name: "Danilo Gallinari",         estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2}
+
+]},
+  { tipodeConferencia: "Este", divicion: "Central",   team: "Chicago Bulls",jugadores:[
+    {tipoDePosicion: "Ala Pivot", name: "Jayson Christopher Tatum", estatura: "2.03 m", Edad: 25, draftdelaNBA: 2017, dorsal: 0},
+    {tipoDePosicion: "Ala Pivot", name: "Al Horford",               estatura: "2.06 m", Edad: 36, draftdelaNBA: 2007, dorsal: 42},
+    {tipoDePosicion: "Ala Pivot", name: "Grant Dean Williams",      estatura: "1.98 m", Edad: 24, draftdelaNBA: 2019, dorsal: 12},
+    {tipoDePosicion: "Ala Pivot", name: "Blake Austin Griffin",     estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2},
+    {tipoDePosicion: "Ala Pivot", name: "Danilo Gallinari",         estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2}
+
+]},
+  { tipodeConferencia: "Este", divicion: "Central",   team: "Cleveland Cavaliers",jugadores:[
+    {tipoDePosicion: "Ala Pivot", name: "Jayson Christopher Tatum", estatura: "2.03 m", Edad: 25, draftdelaNBA: 2017, dorsal: 0},
+    {tipoDePosicion: "Ala Pivot", name: "Al Horford",               estatura: "2.06 m", Edad: 36, draftdelaNBA: 2007, dorsal: 42},
+    {tipoDePosicion: "Ala Pivot", name: "Grant Dean Williams",      estatura: "1.98 m", Edad: 24, draftdelaNBA: 2019, dorsal: 12},
+    {tipoDePosicion: "Ala Pivot", name: "Blake Austin Griffin",     estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2},
+    {tipoDePosicion: "Ala Pivot", name: "Danilo Gallinari",         estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2}
+
+]},
+  { tipodeConferencia: "Este", divicion: "Central",   team: "Detroit Pistons",jugadores:[
+    {tipoDePosicion: "Ala Pivot", name: "Jayson Christopher Tatum", estatura: "2.03 m", Edad: 25, draftdelaNBA: 2017, dorsal: 0},
+    {tipoDePosicion: "Ala Pivot", name: "Al Horford",               estatura: "2.06 m", Edad: 36, draftdelaNBA: 2007, dorsal: 42},
+    {tipoDePosicion: "Ala Pivot", name: "Grant Dean Williams",      estatura: "1.98 m", Edad: 24, draftdelaNBA: 2019, dorsal: 12},
+    {tipoDePosicion: "Ala Pivot", name: "Blake Austin Griffin",     estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2},
+    {tipoDePosicion: "Ala Pivot", name: "Danilo Gallinari",         estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2}
+
+]},
+  { tipodeConferencia: "Este", divicion: "Central",   team: "Indiana Pacers",jugadores:[
+    {tipoDePosicion: "Ala Pivot", name: "Jayson Christopher Tatum", estatura: "2.03 m", Edad: 25, draftdelaNBA: 2017, dorsal: 0},
+    {tipoDePosicion: "Ala Pivot", name: "Al Horford",               estatura: "2.06 m", Edad: 36, draftdelaNBA: 2007, dorsal: 42},
+    {tipoDePosicion: "Ala Pivot", name: "Grant Dean Williams",      estatura: "1.98 m", Edad: 24, draftdelaNBA: 2019, dorsal: 12},
+    {tipoDePosicion: "Ala Pivot", name: "Blake Austin Griffin",     estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2},
+    {tipoDePosicion: "Ala Pivot", name: "Danilo Gallinari",         estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2}
+
+]},
+  { tipodeConferencia: "Este", divicion: "Central",   team: "Milwaukee Bucks",jugadores:[
+    {tipoDePosicion: "Ala Pivot", name: "Jayson Christopher Tatum", estatura: "2.03 m", Edad: 25, draftdelaNBA: 2017, dorsal: 0},
+    {tipoDePosicion: "Ala Pivot", name: "Al Horford",               estatura: "2.06 m", Edad: 36, draftdelaNBA: 2007, dorsal: 42},
+    {tipoDePosicion: "Ala Pivot", name: "Grant Dean Williams",      estatura: "1.98 m", Edad: 24, draftdelaNBA: 2019, dorsal: 12},
+    {tipoDePosicion: "Ala Pivot", name: "Blake Austin Griffin",     estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2},
+    {tipoDePosicion: "Ala Pivot", name: "Danilo Gallinari",         estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2}
+
+]},
+  { tipodeConferencia: "Este", divicion: "Sureste",   team: "Atlanta Hawks",jugadores:[
+    {tipoDePosicion: "Ala Pivot", name: "Jayson Christopher Tatum", estatura: "2.03 m", Edad: 25, draftdelaNBA: 2017, dorsal: 0},
+    {tipoDePosicion: "Ala Pivot", name: "Al Horford",               estatura: "2.06 m", Edad: 36, draftdelaNBA: 2007, dorsal: 42},
+    {tipoDePosicion: "Ala Pivot", name: "Grant Dean Williams",      estatura: "1.98 m", Edad: 24, draftdelaNBA: 2019, dorsal: 12},
+    {tipoDePosicion: "Ala Pivot", name: "Blake Austin Griffin",     estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2},
+    {tipoDePosicion: "Ala Pivot", name: "Danilo Gallinari",         estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2}
+
+]},
+  { tipodeConferencia: "Este", divicion: "Sureste",   team: "Charlotte Hornets",jugadores:[
+    {tipoDePosicion: "Ala Pivot", name: "Jayson Christopher Tatum", estatura: "2.03 m", Edad: 25, draftdelaNBA: 2017, dorsal: 0},
+    {tipoDePosicion: "Ala Pivot", name: "Al Horford",               estatura: "2.06 m", Edad: 36, draftdelaNBA: 2007, dorsal: 42},
+    {tipoDePosicion: "Ala Pivot", name: "Grant Dean Williams",      estatura: "1.98 m", Edad: 24, draftdelaNBA: 2019, dorsal: 12},
+    {tipoDePosicion: "Ala Pivot", name: "Blake Austin Griffin",     estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2},
+    {tipoDePosicion: "Ala Pivot", name: "Danilo Gallinari",         estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2}
+
+]},
+  { tipodeConferencia: "Este", divicion: "Sureste",   team: "Miami Heat",jugadores:[
+    {tipoDePosicion: "Ala Pivot", name: "Jayson Christopher Tatum", estatura: "2.03 m", Edad: 25, draftdelaNBA: 2017, dorsal: 0},
+    {tipoDePosicion: "Ala Pivot", name: "Al Horford",               estatura: "2.06 m", Edad: 36, draftdelaNBA: 2007, dorsal: 42},
+    {tipoDePosicion: "Ala Pivot", name: "Grant Dean Williams",      estatura: "1.98 m", Edad: 24, draftdelaNBA: 2019, dorsal: 12},
+    {tipoDePosicion: "Ala Pivot", name: "Blake Austin Griffin",     estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2},
+    {tipoDePosicion: "Ala Pivot", name: "Danilo Gallinari",         estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2}
+
+]},
+  { tipodeConferencia: "Este", divicion: "Sureste",   team: "Orlando Magic",jugadores:[
+    {tipoDePosicion: "Ala Pivot", name: "Jayson Christopher Tatum", estatura: "2.03 m", Edad: 25, draftdelaNBA: 2017, dorsal: 0},
+    {tipoDePosicion: "Ala Pivot", name: "Al Horford",               estatura: "2.06 m", Edad: 36, draftdelaNBA: 2007, dorsal: 42},
+    {tipoDePosicion: "Ala Pivot", name: "Grant Dean Williams",      estatura: "1.98 m", Edad: 24, draftdelaNBA: 2019, dorsal: 12},
+    {tipoDePosicion: "Ala Pivot", name: "Blake Austin Griffin",     estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2},
+    {tipoDePosicion: "Ala Pivot", name: "Danilo Gallinari",         estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2}
+
+]},
+  { tipodeConferencia: "Este", divicion: "Sureste",   team: "Washington Wizards",jugadores:[
+    {tipoDePosicion: "Ala Pivot", name: "Jayson Christopher Tatum", estatura: "2.03 m", Edad: 25, draftdelaNBA: 2017, dorsal: 0},
+    {tipoDePosicion: "Ala Pivot", name: "Al Horford",               estatura: "2.06 m", Edad: 36, draftdelaNBA: 2007, dorsal: 42},
+    {tipoDePosicion: "Ala Pivot", name: "Grant Dean Williams",      estatura: "1.98 m", Edad: 24, draftdelaNBA: 2019, dorsal: 12},
+    {tipoDePosicion: "Ala Pivot", name: "Blake Austin Griffin",     estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2},
+    {tipoDePosicion: "Ala Pivot", name: "Danilo Gallinari",         estatura: "2.06 m", Edad: 34, draftdelaNBA: 2009, dorsal: 2}
+
+]},
+
+];
+
+for (var i = 0; i < LigaBasketbolNba.length; i++) {
+  var equipo = LigaBasketbolNba[i];
+  
+  console.log("Equipo: " + equipo.team);
+  console.log("Conferencia: " + equipo.TipodeConferencia);
+  console.log("División: " + equipo.divicion);
+  console.log("-----------------------");
+
+  
+  // Verifica si el equipo tiene jugadores
+  if (equipo.jugadores) {
+    console.log("jugadores:");
+    for (var j = 0; j < equipo.jugadores.length; j++) {
+      var jugador = equipo.jugadores[j];
+      
+      console.log("Nombre: " + jugador.name);
+      console.log("Posición: " + jugador.tipoDePosicion);
+      console.log("Estatura: " + jugador.estatura);
+      console.log("Edad: " + jugador.Edad);
+      console.log("Draft de la NBA: " + jugador.draftdelaNBA);
+      console.log("Dorsal: " + jugador.dorsal);
+      console.log("-----------------------");
+    }
+  }
+  
+  console.log("-----------------------");
+}
